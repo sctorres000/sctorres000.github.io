1ef1b27232db50770bbda878612c4b4f62031451
sctorres000 <67390457+sctorres000@users.noreply.github.com>
2020-12-30 20:29:48 -0500

    attempting to make contact page work

---
+++ contact/contact.php
@@ -0,0 +1,21 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_phone = $_POST['phone'];
+$field_message = $_POST['message'];
+
+$mail_to = 'st17@princeton.edu';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Phone: '.$field_phone."\n";
+$body_message .= 'Message: '.$field_message;
+
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+?>
